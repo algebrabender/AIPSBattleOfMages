42740bf6f257efd09f9ca711c09b4399c6c0720c
algebrabender <jelena.firulovic@gmail.com>
2021-12-27 08:52:32 +0100

    organizacija foldera i kreiranje osnova glavnih skripti za interakciju

---
+++ Battle Of Mages/Assets/Scripts/EndGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+
+    private void Awake()
+    {
+       //TODO: postaviti tekst ko je winner
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Battle Of Mages/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        //for editor
+        EditorApplication.isPlaying = false;
+#else
+            //for build
+            Application.Quit();
+#endif
+    }
+}
