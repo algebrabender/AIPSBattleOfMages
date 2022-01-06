ae3cd189d21c358500ffe1b765ab98c31bdd282f
algebrabender <jelena.firulovic@gmail.com>
2022-01-06 09:54:46 +0100

    Editovan .gitignore fajl

--- Battle Of Mages/Assets/Scripts/EndGame.cs
+++ Battle Of Mages/Assets/Scripts/EndGame.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameOver : MonoBehaviour
+public class EndGame : MonoBehaviour
 {
     private void Awake()
     {
